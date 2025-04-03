d523a3dfb5e88b38025b0ee3da0502871bb3eef9
LENAR <0x4C334E3438@users.noreply.github.com>
2025-04-04 01:04:31 +0800

    Update: 4-4-2025

--- custom/files/etc/uci-defaults/notion_system.sh
+++ custom/files/etc/uci-defaults/notion_system.sh
@@ -2,15 +2,29 @@
 #
 # Copyright (c) 2023 LENAR
 #
+
 uci -q batch <<-EOF >/dev/null
+  # Set system properties
   set system.@system[0].hostname="R281"
-  set system.@system[0].timezone="PST-8"
-  set system.@system[0].zonename="Asia/Manila"
-  set system.@system[0].log_proto="udp"
   set system.@system[0].conloglevel="8"
   set system.@system[0].cronloglevel="9"
+
+  # Configure NTP servers
+  del system.ntp.server
+  add_list system.ntp.server='africa.pool.ntp.org'
+  add_list system.ntp.server='asia.pool.ntp.org'
+  add_list system.ntp.server='europe.pool.ntp.org'
+  add_list system.ntp.server='north-america.pool.ntp.org'
+  add_list system.ntp.server='oceania.pool.ntp.org'
+  add_list system.ntp.server='south-america.pool.ntp.org'
+
+  # Luci language
+  set luci.main.lang="auto"
+
+  # Apply changes
   commit system
+  commit ntp
+  commit luci
 EOF
-uci set luci.main.lang="auto" && uci commit luci
 
 exit 0
--- scripts/feeds_pkg.sh
+++ scripts/feeds_pkg.sh
@@ -4,7 +4,7 @@ echo "Remove Packages"
 FOLDER=("small_package" "kenzo" "small")
 for i in "${FOLDER[@]}"; do
 	if [ -d "./feeds/$i" ]; then
-		rm -rif ./feeds/"$i"/{base-files,modemband,luci-app-3ginfo-lite,luci-app-lite-watchdog,luci-app-modemband,luci-app-modemdefine,luci-app-sms-tool,sms-tool,luci-app-xray}
+		rm -rif ./feeds/"$i"/{base-files,ca-certificates,luci-app-3ginfo-lite,luci-app-lite-watchdog,luci-app-modemband,luci-app-modemdefine,luci-app-sms-tool,luci-app-v2raya,luci-app-xray,modemband,sms-tool,v2fly-geodata,v2ray-core,v2raya,xray-core}
 	fi
 done
 
