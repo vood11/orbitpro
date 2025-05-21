9b87b8baa01f8c282898f6269abd3b2b670766cf
LENAR <0x4C334E3438@users.noreply.github.com>
2025-05-21 22:33:53 +0800

    Update: 5-21-2025

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -14,6 +14,7 @@ src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
 src-git sms_tool https://github.com/4IceG/luci-app-sms-tool
 src-git xray https://github.com/yichya/luci-app-xray
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
+src-git zapret https://github.com/remittor/zapret-openwrt
 EOF
 
 exit 0
