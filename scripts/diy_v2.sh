adb0251b946e576cc0f7d5567d89d3f500334bad
vood11 <rosyib_id@yahoo.com>
2025-09-02 01:04:21 +0700

    Update diy_v2.sh

--- scripts/diy_v2.sh
+++ scripts/diy_v2.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "Move Custom Files"
-mv -f ./custom/files ./
+#=================================================
+# Description: Minimalist DIY script
+#=================================================
 
-echo "Patch For NCM"
+### BAGIAN 1: KUSTOMISASI INFO BUILD KERNEL (UNTUK UNAME -A) ###
+echo "Setting custom kernel build information..."
+BUILD_INFO="dbai@tim-mawar Build on $(date -d '1998-05-04' +'%Y-%m-%d')"
+sed -i "s/^\(LINUX_KERNEL_TESTING_VERSION :=\).*/\1 ${BUILD_INFO}/" include/version.mk
+
+---
+
+### BAGIAN 2: PERINTAH ASLI DARI SCRIPT ANDA ###
+echo "Moving custom files and patching NCM..."
+mv -f ./custom/files ./
 mv -f ./custom/patch/ncm.* ./package/network/utils/comgt/files/
 
-echo "Remove Config Folder"
-rm -r ./files/etc/config
-
+echo "DIY script finished."
 exit 0
