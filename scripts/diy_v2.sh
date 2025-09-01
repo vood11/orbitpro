4c980036037b21a33c96cbe35530a7af9cd9f9fb
vood11 <rosyib_id@yahoo.com>
2025-09-02 06:09:29 +0700

    Update diy_v2.sh

--- scripts/diy_v2.sh
+++ scripts/diy_v2.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#=================================================
-# Description: Minimalist DIY script
-#=================================================
-
-### BAGIAN 1: KUSTOMISASI INFO BUILD KERNEL (UNTUK UNAME -A) ###
-echo "Setting custom kernel build information..."
-BUILD_INFO="dbai@tim-mawar Build on $(date -d '1998-05-04' +'%Y-%m-%d')"
-sed -i "s/^\(LINUX_KERNEL_TESTING_VERSION :=\).*/\1 ${BUILD_INFO}/" include/version.mk
-
----
-
-### BAGIAN 2: PERINTAH ASLI DARI SCRIPT ANDA ###
 echo "Moving custom files and patching NCM..."
 mv -f ./custom/files ./
 mv -f ./custom/patch/ncm.* ./package/network/utils/comgt/files/
