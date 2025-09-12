c8f104a81314289fa9d70f43290ca799234450ae
vood11 <rosyib_id@yahoo.com>
2025-09-12 15:44:13 +0700

    Update diy_v2.sh

--- scripts/diy_v2.sh
+++ scripts/diy_v2.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo "Moving custom files and patching NCM..."
-mv -f ./custom/files ./
-mv -f ./custom/patch/ncm.* ./package/network/utils/comgt/files/
+# Menghitung tanggal yang benar dalam format UTC
+EPOCH_DATE=$(date -d '1998-05-04 00:00:00 UTC' +%s)
 
-echo "DIY script finished."
+# Menulis ulang skrip epoch dengan metode yang lebih aman dan eksplisit
+echo '#!/bin/sh' > scripts/get_source_date_epoch.sh
+echo "echo ${EPOCH_DATE}" >> scripts/get_source_date_epoch.sh
+chmod +x scripts/get_source_date_epoch.sh
+
+# Perintah asli Anda
+mv -f ./custom/files ./
+
 exit 0
