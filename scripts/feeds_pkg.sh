e83dd705a6def470e73eb518472c3bbad61d6ab2
vood11 <rosyib_id@yahoo.com>
2025-09-15 22:32:16 +0700

    Update feeds_pkg.sh

--- scripts/feeds_pkg.sh
+++ scripts/feeds_pkg.sh
@@ -4,7 +4,7 @@ echo "Remove Packages"
 FOLDER=("small_package" "openwrt_packages" "small")
 for i in "${FOLDER[@]}"; do
 	if [ -d "./feeds/$i" ]; then
-		rm -rif ./feeds/"$i"/{base-files,ca-certificates,luci-proto-xmm,xmm-modem}
+		rm -rif ./feeds/"$i"/{base-files,ca-certificates,luci-proto-xmm,xmm-modem,luci-app-openclash}
 	fi
 done
 
