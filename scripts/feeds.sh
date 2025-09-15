1d3c1c519704e5d3482a75782f2d3e3145013a58
vood11 <rosyib_id@yahoo.com>
2025-09-15 22:23:18 +0700

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -2,26 +2,17 @@
 
 cat >> ./feeds.conf.default <<EOF
 # Extra Feeds
-src-git small_package https://github.com/vood11/small-package
+# src-git small_package https://github.com/vood11/small-package
 # src-git openwrt_packages https://github.com/kenzok8/openwrt-packages
 # src-git small https://github.com/kenzok8/small
+src-git kwrt-packages https://github.com/kiddin9/kwrt-packages
 
 # Extra Packages
-src-git irqbalance https://github.com/4IceG/luci-app-irqbalance
-src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
-src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
 src-git my_pkg https://github.com/vood11/My_PKG
-src-git modemband https://github.com/4IceG/luci-app-modemband
-src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
-src-git sms_tool https://github.com/4IceG/luci-app-sms-tool
-src-git xray https://github.com/yichya/luci-app-xray
 src-git xmm https://github.com/eichgee/xmm-modem-mod.git
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
 src-git zapret https://github.com/remittor/zapret-openwrt
 src-git openclash https://github.com/vernesong/OpenClash.git;v0.46.086
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git
-src-git passpaket https://github.com/xiaorouji/openwrt-passwall-packages.git
-src-git neko https://github.com/nosignals/openwrt-neko.git;main
 EOF
 
 exit 0
