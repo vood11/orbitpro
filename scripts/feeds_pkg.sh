0fc65a89dadb02011d2f52ef25e9305ea534a81f
vood11 <rosyib_id@yahoo.com>
2025-09-15 22:31:04 +0700

    Update feeds_pkg.sh

--- scripts/feeds_pkg.sh
+++ scripts/feeds_pkg.sh
@@ -4,7 +4,7 @@ echo "Remove Packages"
 FOLDER=("small_package" "openwrt_packages" "small")
 for i in "${FOLDER[@]}"; do
 	if [ -d "./feeds/$i" ]; then
-		rm -rif ./feeds/"$i"/{base-files,ca-certificates,luci-app-3ginfo-lite,luci-app-lite-watchdog,luci-app-modemband,luci-app-modemdefine,luci-app-sms-tool,luci-app-v2raya,luci-app-xray,modemband,sms-tool,v2fly-geodata,v2ray-core,v2raya,xray-core}
+		rm -rif ./feeds/"$i"/{base-files,ca-certificates,luci-proto-xmm,xmm-modem}
 	fi
 done
 
